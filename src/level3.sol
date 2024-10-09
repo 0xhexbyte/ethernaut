53ac25b843da4009c6eda37211be371d8e952f2a
0xhexbyte <mrigendrasoni26@gmail.com>
2024-10-09 06:14:15 -0400

    adding stuff

--- src/level3.sol
+++ src/level3.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8;
 
 import "contracts/level3.sol";
 
+interface ICoinFlip {
+    function consecutiveWins() external view returns (uint256);
+    function flip(bool) external returns (bool);
+}
+
 contract Hack {
+    ICoinFlip private immutable target = ICoinFlip(0x36ce7540571705D1168740b92d1ce0f0fAe95Cdf);
     uint256 private constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    CoinFlip target = CoinFlip(0x36ce7540571705D1168740b92d1ce0f0fAe95Cdf);
 
     // call this function 10 times
     function flip() external {
